7dc2556328dbcc26c28cd50db34fe744109e9994
Evgeniy Scherbakov <evgeniy.scherbakov@gmail.com>
2015-03-02 02:47:58 +0300

    master: Step 7

--- app/js/views/contact.js
+++ app/js/views/contact.js
@@ -4,10 +4,23 @@ define(['underscore', 'backbone', 'text!templates/tpl-contact.html'], function (
         className: 'media col-md-6 col-lg-4',
         template: _.template(contactTpl),
 
+        events: {
+            'click .delete-contract': 'onClickDelete'
+        },
+
+        initialize: function () {
+            this.listenTo(this.model, 'remove', this.remove);
+        },
+
         render: function () {
             var html = this.template(this.model.toJSON());
             this.$el.append(html);
             return this;
+        },
+
+        onClickDelete: function (e) {
+            e.preventDefault();
+            this.model.collection.remove(this.model);
         }
     });
     return Contact;
