9337bbfdb3adb61cca2386acfd0bd80bc729e951
Evgeniy Scherbakov <evgeniy.scherbakov@gmail.com>
2015-03-02 00:20:54 +0300

    master: Step 1

---
+++ app/js/require.cfg.js
@@ -0,0 +1,9 @@
+require.config({
+    baseUrl: "",
+    paths: {
+        'jquery': '/vendor/jquery/dist/jquery.min',
+        'underscore': '/vendor/underscore/underscore',
+        'backbone': '/vendor/backbone/backbone',
+        'text': '/vendor/requirejs-text/text'
+    }
+});
